811646ce128465c0d2a6803daaf2584d5aba1f0d
haneenMahmoud12 <101831324+haneenMahmoud12@users.noreply.github.com>
2023-12-22 15:24:07 +0200

    This commit has a problem

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -475,7 +475,7 @@ namespace StarterAssets
             _animator.SetBool("Pick", false);
 
         }
-        private void DelayedActionForGold(Collider other)
+        private void DelayedActionForGold()
         {
             _animator.SetBool("Pick", false);
 
